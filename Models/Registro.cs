23c551b037f38f2eb093aa313da307d670a4d59e
lohammy vasquez adames <2005lohammy@gmail.com>
2025-09-22 20:16:43 -0400

    se creo el modelo

---
+++ Models/Registro.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace P1_Ap1_LohammyVasquez.Models;
+
+
+public class Registro
+{
+    [Key]
+    public int id { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+
+    public string name { get; set; }
+}
+

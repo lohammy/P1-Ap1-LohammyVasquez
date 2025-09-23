8baff7dcc91c8d5a6a1c15bc6b6d434c5f6c75ae
lohammy vasquez adames <2005lohammy@gmail.com>
2025-09-22 20:14:11 -0400

    se creo el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_Ap1_LohammyVasquez.Models;
+
+namespace P1_Ap1_LohammyVasquez.DAL;
+
+public class Contexto : DbContext
+{
+    public DbSet<Registro> Registro { get; set; }
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+}

c2c5715418bff888496244012e6b5b823309ae1c
lohammy vasquez adames <2005lohammy@gmail.com>
2025-09-29 22:04:28 -0400

    cree el modelo

--- Models/EntradaHuacales.cs
+++ Models/EntradaHuacales.cs
@@ -1,5 +1,10 @@
 ﻿
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using P1_Ap1_LohammyVasquez.DAL;
+using P1_Ap1_LohammyVasquez.Models;
 
 namespace P1_Ap1_LohammyVasquez.Models;
 
@@ -7,10 +12,21 @@ namespace P1_Ap1_LohammyVasquez.Models;
 public class EntradaHuacales
 {
     [Key]
-    public int id { get; set; }
+    public int IdEntrada { get; set; }
 
     [Required(ErrorMessage = "Este campo es obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+    public string NombreCliente { get; set; }
 
-    public string name { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+    
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [Range(1, double.MaxValue, ErrorMessage = "Debe introducir una cantidad valida")]
+    public int Cantidad { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [Range(1, double.MaxValue, ErrorMessage = "Debe introducir un monto valido")]
+    public decimal Precio { get; set; }
 }
 
